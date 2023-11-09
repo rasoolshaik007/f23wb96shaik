7b0aa948bf13019184ed46d2b94467b136d8cdf9
Rasool Shaik <rasoolshaik957307@gmail.com>
2023-11-09 04:05:00 -0600

    controller

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var flight = require("./models/flight");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var flightRouter = require('./routes/flight');
+var resourceRouter = require('./routes/resource');
 
+require('dotenv').config();
+const connectionString =process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
 var app = express();
 
 // view engine setup
@@ -23,11 +28,60 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flight', flightRouter);
+app.use('/resource',resourceRouter);
+app.use();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")})
+//We can seed the collection if needed on
+async function recreateDB(){
+  
+await flight.deleteMany();
+let instance1 = new flight({flight_name:"angerLove", flight_author:"vivekanandha", flight_pages:233});
+instance1.save().then(doc=>{
+console.log("First object saved")}
+).catch(err=>{
+console.error(err)
+});
+
+let instance2 = new flight({flight_name:"angerLove", flight_author:"vivekanandha", flight_pages:233});
+instance2.save().then(doc=>{
+console.log("Second object saved")}
+).catch(err=>{
+console.error(err)
+});
+
+let instance3 = new flight({flight_name:"wings of freedom", flight_author:"abdul kalam", flight_pages:103});
+instance3.save().then(doc=>{
+console.log("Third object saved")}
+).catch(err=>{
+console.error(err)
+});
+}
+let reseed = true;
+if (reseed) {recreateDB();
+}
+// List of all Costumes
+exports.flight_list = async function(req, res) {
+try{
+  console.log(`Triggered`);
+theflight = await flight.find();
+res.send(theflight);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ controllers/flight.js
@@ -0,0 +1,21 @@
+var flight = require('../models/flight');
+// List of all flights
+exports.flight_list = function(req, res) {
+res.send('NOT IMPLEMENTED: flight list');
+};
+// for a specific flight.
+exports.flight_detail = function(req, res) {
+res.send('NOT IMPLEMENTED: flight detail: ' + req.params.id);
+};
+// Handle flight create on POST.
+exports.flight_create_post = function(req, res) {
+res.send('NOT IMPLEMENTED: flight create POST');
+};
+// Handle flight delete form on DELETE.
+exports.flight_delete = function(req, res) {
+res.send('NOT IMPLEMENTED: flight delete DELETE ' + req.params.id);
+};
+// Handle flight update form on PUT.
+exports.flight_update_put = function(req, res) {
+res.send('NOT IMPLEMENTED: flight update PUT' + req.params.id);
+};
