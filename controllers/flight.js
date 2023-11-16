8ea757b1bb12ec44d7ee109e3b4c001a34c84d97
Rasool Shaik <rasoolshaik957307@gmail.com>
2023-11-15 23:48:41 -0600

    added commits

--- controllers/flight.js
+++ controllers/flight.js
@@ -99,14 +99,57 @@ exports.flight_view_one_Page = async function(req, res) {
 console.log("single view for id " + req.query.id)
 try{
 result = await flight.findById( req.query.id)
-res.render('flightdetail',
-{ title: 'flight Detail', toShow: result });
+res.render('flightdetail',{ title: 'Flight Detail', toShow: result });
 }
 catch(err){
 res.status(500)
 res.send(`{'error': '${err}'}`);
 }
 };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.flight_create_Page = function(req, res) {
+console.log("create view")    
+try{
+res.render('flightcreate', { title: 'Flight Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+//Handle building the view for updating a costume.
+// query provides the id
+exports.flight_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await flight.findById(req.query.id)
+res.render('flightupdate', { title: 'Flight Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle a delete one view with id from query
+exports.flight_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await flight.findById(req.query.id)
+res.render('flightdelete', { title: 'Flight Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
 
 // List of all flights
 // exports.flight_list = function(req, res) {
--- routes/flight.js
+++ routes/flight.js
@@ -9,4 +9,15 @@ router.get('/flight/:id', flight_controllers.flight_detail);
 
 /* GET detail flight page */
 router.get('/detail', flight_controllers.flight_view_one_Page);
+
+/* GET create flight page */
+router.get('/create', flight_controllers.flight_create_Page);
+
+// GET request to update Flight.
+router.get('/update', flight_controllers.flight_update_Page);
+
+/* GET delete flight page */
+router.get('/delete', flight_controllers.flight_delete_Page);
+
+
 module.exports = router;
