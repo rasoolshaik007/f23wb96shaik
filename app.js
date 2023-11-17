83ae2d121b7b44fb636a7955f1493ef218ec0ccd
Rasool Shaik <rasoolshaik957307@gmail.com>
2023-11-17 05:35:00 -0600

    update button

--- app.js
+++ app.js
@@ -8,7 +8,33 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var flightRouter = require('./routes/flight');
 var resourceRouter = require('./routes/resource');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 require('dotenv').config();
 const connectionString =process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -24,7 +50,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/flight', flightRouter);
